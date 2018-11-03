6d3dd1a6684345d61e4539ca85b2d8ba863f0f8a
George Daramouskas <george.daramouskas@gmail.com>
2018-11-03 18:31:05 +0100

    [ADD] l10n-gr-bank

---
+++ l10n_gr_bank/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 bitwise.solutions <http://bitwise.solutions>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Greek banks list",
+    "version": "10.0.1.0.0",
+    "author": "bitwise.solutions",
+    "license": "AGPL-3",
+    "category": "Localisation",
+    "summary": "Import all Greek banks with BIC code",
+    "depends": [
+	"base",
+    ],
+    "data": [
+        'data/res_bank_data.xml',
+    ],
+    "installable": True,
+}
